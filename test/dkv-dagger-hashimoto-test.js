2e313be322bd24cd7a6d5bc32e7c8a920e8fd486
cyl19970726 <15258378443@163.com>
2023-04-01 00:39:44 +0800

    test for checkDaggerDataWithProof()

--- test/dkv-dagger-hashimoto-test.js
+++ test/dkv-dagger-hashimoto-test.js
@@ -2,6 +2,7 @@ const { web3 } = require("hardhat");
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 const  crypto  = require("crypto");
+const { constants } = require("buffer");
 
 var ToBig = (x) => ethers.BigNumber.from(x);
 var padRight32 = (x) => x.padEnd(66, "0");
@@ -18,7 +19,7 @@ var hexlen = (str) => ethers.utils.hexDataLength(str);
 describe("Basic Func Test", function () {
   it("hashimoto-tiny", async function () {
     const SystemContract = await ethers.getContractFactory("TestSystemContractDaggerHashimoto");
-    const sc = await SystemContract.deploy();
+    const sc = await SystemContract.deploy(4096);
 
     const MinabledKV = await ethers.getContractFactory("TestDecentralizedKVDaggerHashimoto");
     // 64 bytes per data, 4 entries in shard, 1 random access
@@ -31,6 +32,7 @@ describe("Basic Func Test", function () {
     );
     await kv.deployed();
 
+    // put 16 kv-datas 
     for (let i = 0; i < 16; i++) {
       await kv.put(ethers.utils.formatBytes32String(i.toString()), hexlify64(i));
     }
@@ -48,7 +50,7 @@ describe("Basic Func Test", function () {
 
   it("hashimoto-small", async function () {
     const SystemContract = await ethers.getContractFactory("TestSystemContractDaggerHashimoto");
-    const sc = await SystemContract.deploy();
+    const sc = await SystemContract.deploy(4096);
 
     const MinabledKV = await ethers.getContractFactory("TestDecentralizedKVDaggerHashimoto");
     // 64 bytes per data, 4 entries in shard, 2 random access
@@ -81,7 +83,7 @@ describe("Basic Func Test", function () {
 
   it("hashimoto-large", async function () {
     const SystemContract = await ethers.getContractFactory("TestSystemContractDaggerHashimoto");
-    const sc = await SystemContract.deploy();
+    const sc = await SystemContract.deploy(4096);
 
     const MinabledKV = await ethers.getContractFactory("TestDecentralizedKVDaggerHashimoto");
     // 4096 bytes per data, 32 entries in shard, 16 random access
@@ -149,7 +151,7 @@ describe("Basic Func Test", function () {
 
   it("hashimoto-large-keccak", async function () {
     const SystemContract = await ethers.getContractFactory("TestSystemContractDaggerHashimoto");
-    const sc = await SystemContract.deploy();
+    const sc = await SystemContract.deploy(4096);
 
     const MinabledKV = await ethers.getContractFactory("TestDecentralizedKVDaggerHashimoto");
     // 4096 bytes per data, 32 entries in shard, 16 random access
@@ -198,7 +200,7 @@ describe("Basic Func Test", function () {
 
   it("calculateDiffAndInitHash", async function () {
     const SystemContract = await ethers.getContractFactory("TestSystemContractDaggerHashimoto");
-    const sc = await SystemContract.deploy();
+    const sc = await SystemContract.deploy(4096);
     await sc.deployed();
     const MinabledKV = await ethers.getContractFactory("TestDecentralizedKVDaggerHashimoto");
     const kv = await MinabledKV.deploy([5, 5, 8, 6, 10, 60, 40, 1024, 0, sc.address], 0, 0, 0, formatB32Str("genesis"));
@@ -228,6 +230,7 @@ describe("Full cycle of mining procedure", function () {
   const cutoff = 40;
   const diffAdjDivisor = 1024;
   const coinbaseShare = 0;
+  // shardEntryBits is the number of KV included in the shard.  
   const shardEntryBits = shardSizeBits - maxKvSizeBits;
   const verbose = false;
 
@@ -238,6 +241,7 @@ describe("Full cycle of mining procedure", function () {
   /* Conduct a off-chain pre-check for gaining reward purpose */
   async function hashimoto_local(startShardId, shardLenBits, _hash0, maskedData, randomList, kv, sc) {
     const maxKvSize = 1 << maxKvSizeBits;
+    // shardLenBits means the number of chunk which engage in the mint-progress for miner to find a valid difficulty.
     const rows = 1 << (shardEntryBits + shardLenBits);
     let hash0 = _hash0;
     for (let i = 0; i < randomChecks; i++) {
@@ -291,7 +295,7 @@ describe("Full cycle of mining procedure", function () {
     const [owner, addr1, addr2] = await ethers.getSigners();
     let wallet = ethers.Wallet.createRandom().connect(owner.provider);
     const SystemContract = await ethers.getContractFactory("TestSystemContractDaggerHashimoto");
-    const sc = await SystemContract.deploy();
+    const sc = await SystemContract.deploy(4096);
 
     const MinabledKV = await ethers.getContractFactory("TestDecentralizedKVDaggerHashimoto");
     // 64 bytes per data, 16 entries in shard, 4 random access
@@ -395,20 +399,20 @@ describe("Full cycle of mining procedure with Merkle proof", function () {
   /* Conduct a off-chain pre-check for gaining reward purpose */
   function hashimoto_local(startShardId, shardLenBits, _hash0, maskedData, randomList, chunkIdArray, chunkLenBits) {
     const maxKvSize = 1 << maxKvSizeBits;
-    const rows = 1 << (shardEntryBits + shardLenBits + chunkLenBits);
+    const rows = 1 << (shardEntryBits + shardLenBits + chunkLenBits);// shardNums * shardChunks *
     let hash0 = _hash0;
     for (let i = 0; i < randomChecks; i++) {
         const parent = ethers.BigNumber.from(hash0).mod(rows).toNumber();
-        const chunkOffset = parent + (startShardId << (shardEntryBits + chunkLenBits));
-        const kvIdx = chunkOffset >> chunkLenBits;
-        const chunkIdx = chunkOffset % (1 << chunkLenBits);
+        const sampleChunkIdx = parent + (startShardId << (shardEntryBits + chunkLenBits));
+        const kvIdx = sampleChunkIdx >> chunkLenBits;
+        const chunkLeafIdx = sampleChunkIdx % (1 << chunkLenBits);
         if (verbose){
           console.log("hash is "+hash0);
           console.log("kvIdx %d -- desired randomList idx %d; chunkIdx %d -- desired id from array %d ", 
-                      kvIdx, randomList[i], chunkIdx, chunkIdArray[i]);
+                      kvIdx, randomList[i], chunkLeafIdx, chunkIdArray[i]);
         }
         // in shard local data, index matches = data matches
-        if (kvIdx != randomList[i] || chunkIdx != chunkIdArray[i]) {
+        if (kvIdx != randomList[i] || chunkLeafIdx != chunkIdArray[i]) {
           return [0, false];
         }
         hash0 = keccak256(concat([padRight32(ethers.utils.hexlify(hash0)), maskedData[i]]));
@@ -422,11 +426,146 @@ describe("Full cycle of mining procedure with Merkle proof", function () {
     return numbers.slice(0,randomChecks);
   }
 
+  it("test checkDaggerDataWithProof()",async function(){
+    const [owner, addr1, addr2] = await ethers.getSigners();
+    let wallet = ethers.Wallet.createRandom().connect(owner.provider);
+    const SystemContract = await ethers.getContractFactory("TestSystemContractDaggerHashimoto");
+    const sc = await SystemContract.deploy(2048);
+    await sc.deployed();
+
+    const MinabledKV = await ethers.getContractFactory("TestDecentralizedKVDaggerHashimoto");
+    // 32KB per KV block, 4 entries in shard, 1 random access
+    const _chunkSizeBits = 11; // 2048
+    const kv = await MinabledKV.deploy(
+      [maxKvSizeBits, _chunkSizeBits, shardSizeBits, randomChecks, minimumDiff, 
+       targetIntervalSec, cutoff, diffAdjDivisor, coinbaseShare, sc.address],
+      0, //startTime
+      0, //storageCost
+      0, // dcfFactor
+      ethers.utils.formatBytes32String("") //genesisHash
+    );
+    await kv.deployed();
+
+    const MerkleLib = await ethers.getContractFactory("TestMerkleLib");
+    const ml = await MerkleLib.deploy();
+    await ml.deployed();
+    
+    const numShards = 1 << (shardSizeBits - maxKvSizeBits);
+    let l = 0;
+    let dataList = [];
+
+    const dataSize = 6000;
+    // case 1:put a whole kvSize data 6000, the maxKvSize is 8192 , the chunkSize is 2048
+    // chunk1 size = 2048 ; chunk2 size = 2048 ; chunk3 size =1904 ; chunk4 size = 0 
+    const kvIdx_0 = 0 
+    const d = crypto.randomBytes(dataSize);
+    dataList.push(d);
+    // PUT won't change the diff info
+    await kv.put(local_idx_2_key(kvIdx_0), ethers.utils.hexlify(dataList[kvIdx_0]));
+
+    console.log(dataList[kvIdx_0].length)
+
+    const getData = await kv.get(local_idx_2_key(kvIdx_0),0,dataSize)
+    const expectData = ToBig(dataList[kvIdx_0]).toHexString();
+    expect(getData).to.eq(expectData);
+
+    const kvInfo_0 = await kv.getKVInfo(kvIdx_0)
+    
+    const CHUNK_SIZE = (await kv.chunkSize()).toNumber();
+    const chunkLenBits = (await kv.chunkLenBits()).toNumber();
+
+    // sample chunk 0 
+    const SAMPLE_CHUNK_0 = 0;
+    const proof_0= await ml.getProof(dataList[kvIdx_0],CHUNK_SIZE,chunkLenBits,SAMPLE_CHUNK_0)
+    let chunkData_0 = Buffer.alloc(CHUNK_SIZE)
+    let startChunkPtr = 0
+    dataList[kvIdx_0].copy(chunkData_0,0,startChunkPtr,CHUNK_SIZE);
+    let result_0 = await kv.checkDaggerDataWithProof(SAMPLE_CHUNK_0,kvInfo_0,proof_0,chunkData_0)
+    expect(result_0).to.eq(true);
+
+    // sample chunk 1
+    const SAMPLE_CHUNK_1 = 1;
+    const proof_1= await ml.getProof(dataList[kvIdx_0],CHUNK_SIZE,chunkLenBits,SAMPLE_CHUNK_1)
+    let chunkData_1 = Buffer.alloc(CHUNK_SIZE,0)
+    startChunkPtr = CHUNK_SIZE  + startChunkPtr
+    dataList[kvIdx_0].copy(chunkData_1,0,startChunkPtr,startChunkPtr+CHUNK_SIZE);
+    let result_1 = await kv.checkDaggerDataWithProof(SAMPLE_CHUNK_1,kvInfo_0,proof_1,chunkData_1)
+    expect(result_1).to.eq(true);
+
+    // sample chunk 2 
+    const SAMPLE_CHUNK_2 = 2;
+    const proof_2 = await ml.getProof(dataList[kvIdx_0],CHUNK_SIZE,chunkLenBits,SAMPLE_CHUNK_2)
+    let chunkData_2 = Buffer.alloc(CHUNK_SIZE,0)
+    startChunkPtr += CHUNK_SIZE 
+    dataList[kvIdx_0].copy(chunkData_2,0,startChunkPtr,startChunkPtr+1904);
+    let result_2 = await kv.checkDaggerDataWithProof(SAMPLE_CHUNK_2,kvInfo_0,proof_2,chunkData_2)
+    expect(result_2).to.eq(true);
+
+    //sample chunk 3 
+    const SAMPLE_CHUNK_3 = 3;
+    const proof_3= await ml.getProof(dataList[kvIdx_0],CHUNK_SIZE,chunkLenBits,SAMPLE_CHUNK_3)
+    let chunkData_3 = Buffer.alloc(CHUNK_SIZE,0)
+    let result_3 = await kv.checkDaggerDataWithProof(SAMPLE_CHUNK_3,kvInfo_0,proof_3,chunkData_3)
+    expect(result_3).to.eq(true);
+
+    // sample chunk 4 
+    {
+      const SAMPLE_CHUNK_4 = 4;
+      let chunkData_4 = Buffer.alloc(CHUNK_SIZE,0)
+      let result_4 = await kv.checkDaggerDataWithProof(SAMPLE_CHUNK_4,kvInfo_0,[],chunkData_4)
+      expect(result_4).to.eq(true);
+      let chunkData_4_fail = Buffer.alloc(CHUNK_SIZE,1)
+      result_4 = await kv.checkDaggerDataWithProof(SAMPLE_CHUNK_4,kvInfo_0,[],chunkData_4_fail)
+      expect(result_4).to.eq(false);
+
+    }
+
+
+    // ======== put the kvIdx 1 ============= 
+    const datasize_1 = 4096
+    const kvIdx_1 = 1 
+    const d1 = crypto.randomBytes(datasize_1);
+    dataList.push(d1);
+    // PUT won't change the diff info
+    await kv.put(local_idx_2_key(kvIdx_1), ethers.utils.hexlify(dataList[kvIdx_1]));
+
+    const getData1 = await kv.get(local_idx_2_key(kvIdx_1),0,datasize_1)
+    const expectData1 = ToBig(dataList[kvIdx_1]).toHexString();
+    expect(getData1).to.eq(expectData1);
+
+    const kvInfo_1 = await kv.getKVInfo(kvIdx_1)
+    let chunksNumPerKV = 1 << chunkLenBits
+
+    // sample ChunkIdx 4 
+    // const SAMPLE_CHUNK_4 = 4;
+    // const proof_4= await ml.getProof(dataList[kvIdx_1],CHUNK_SIZE,chunkLenBits,SAMPLE_CHUNK_4%chunksNumPerKV)
+    // let chunkData_4 = Buffer.alloc(CHUNK_SIZE,0)
+    // dataList[kvIdx_1].copy(chunkData_4,0,0,CHUNK_SIZE);
+    // let result_4 = await kv.checkDaggerDataWithProof(SAMPLE_CHUNK_4,kvInfo_1,proof_4,chunkData_4)
+    // expect(result_4).to.eq(true);
+
+    // // sample ChunkIdx 5 
+    // const SAMPLE_CHUNK_5 = 5;
+    // const proof_5 = await ml.getProof(dataList[kvIdx_1],CHUNK_SIZE,chunkLenBits,SAMPLE_CHUNK_5%chunksNumPerKV)
+    // const chunkData_5 = Buffer.alloc(CHUNK_SIZE,0);
+    // dataList[kvIdx_1].copy(chunkData_5,0,CHUNK_SIZE,2*CHUNK_SIZE);
+    // let result_5 = await kv.checkDaggerDataWithProof(SAMPLE_CHUNK_5,kvInfo_1,proof_5,chunkData_5,)
+    // expect(result_5).to.eq(true);
+
+
+    // sample ChunkIdx 6
+    const SAMPLE_CHUNK_6 = 6;
+    let chunkData_6 = Buffer.alloc(CHUNK_SIZE,0)
+    let result_6 = await kv.checkDaggerDataWithProof(SAMPLE_CHUNK_6,kvInfo_1,[],chunkData_6)
+    expect(result_6).to.eq(true);
+
+  })
+
   it("runs full mining cycle 32KB", async function () {
     const [owner, addr1, addr2] = await ethers.getSigners();
     let wallet = ethers.Wallet.createRandom().connect(owner.provider);
     const SystemContract = await ethers.getContractFactory("TestSystemContractDaggerHashimoto");
-    const sc = await SystemContract.deploy();
+    const sc = await SystemContract.deploy(4096);
     await sc.deployed();
 
     const MinabledKV = await ethers.getContractFactory("TestDecentralizedKVDaggerHashimoto");
