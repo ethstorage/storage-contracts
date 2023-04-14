2d34dac90d05eb369792655e28402c143ace66c2
cyl19970726 <15258378443@163.com>
2023-04-14 12:29:07 +0800

    update run-scripts

--- run-scripts/get-dkv-runtime-code.js
+++ run-scripts/get-dkv-runtime-code.js
@@ -3,6 +3,13 @@ const { ethers } = require("hardhat");
 
 
 async function main(){
+    const getFac = await ethers.getContractFactory(
+      "GetRuntimeCode"
+    );
+
+    const get = await getFac.deploy()
+    await get.deployed();
+
     const MinabledKV = await ethers.getContractFactory(
         "DecentralizedKVDaggerHashimoto"
       );
@@ -11,29 +18,37 @@ async function main(){
       //maxKvSizeBits chunkSizeBits  shardSizeBits(18)
       // const kv = await MinabledKV.deploy(
       //   [17, 12, 35, 16, 100, 300, 40, 1024, 1000, scAddr],
+      //   1,
       //   0,
       //   0,
-      //   0,
-      //   ethers.utils.formatBytes32String("")
+      //   ethers.utils.formatBytes32String("0x1111111111111111111111111111111111111111111111111111111111111111")
       // );
       const kv = await MinabledKV.deploy(
-        [17, 12, 27, 16, 100, 300, 40, 1024, 1000, scAddr],
+        [17, 12, 25, 16, 100, 300, 40, 1024, 1000, "0x0000000000000000000000000000000003330003"],
+        1,
         0,
         0,
-        0,
-        ethers.utils.formatBytes32String("")
+        "0x1111111111111111111111111111111111111111111111111111111111111111"
       );
       // const kv = await MinabledKV.deploy(
       //   [12, 12, 18, 16, 100, 300, 40, 1024, 1000, scAddr],
+      //   1,
       //   0,
       //   0,
-      //   0,
-      //   ethers.utils.formatBytes32String("")
+      //   "0x1111111111111111111111111111111111111111111111111111111111111111"
       // );
       await kv.deployed();
 
       let code = await ethers.provider.getCode(kv.address);
+
+      let runtime_code = await get.getcode(kv.address);
+
+      let sInfo = await kv.infos(0)
+      console.log(sInfo);
+      console.log("============code==============")
       console.log(code)
+      console.log("============runtime code==============")
+      console.log(runtime_code)
 }
 
 main().catch((error) => {
