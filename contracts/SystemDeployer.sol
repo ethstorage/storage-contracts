c4212ae81c5c5dc4efe4f90d4ae77d6b9732593d
syntrust <lundeng@quarkchain.org>
2023-04-18 16:07:26 +0800

    init mining

--- contracts/SystemDeployer.sol
+++ contracts/SystemDeployer.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.8.0;
 
 // DKVHash 0x0000000000000000000000000000000003330001
 // PrecompileManager 0x0000000000000000000000000000000003330003
-contract deployer {
+contract SystemDeployer {
     address public constant systemDeployer = 0x0000000000000000000000000000000000033301;
 
     string public errmsg;
